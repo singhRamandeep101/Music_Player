26c0b447a0d6277614fd4135bf396960ba6f9a46
singhRamandeep101 <158157425+singhRamandeep101@users.noreply.github.com>
2025-04-26 18:57:52 +0800

    working add music to database

--- src/database.py
+++ src/database.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 class MusicPlayerDatabase:
-    def __init__(self, db_name = "data/musiclibrary.db"):
+    def __init__(self, db_name="data/musiclibrary.db"):
         self.db_name = db_name
         self.init_db()
 
@@ -11,12 +11,25 @@ class MusicPlayerDatabase:
             cursor.execute("""
                 CREATE TABLE IF NOT EXISTS music (
                     id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    tille TEXT NOT NULL,
+                    title TEXT NOT NULL,
                     artist TEXT NOT NULL,
                     mp3_link TEXT NOT NULL
-                    )
-                """)
+                )
+            """)
             conn.commit()
+
+    def add_music(self, title, artist, mp3_link):
+        try:
+            with sqlite3.connect(self.db_name) as conn:
+                cursor = conn.cursor()
+                cursor.execute("INSERT INTO music(title, artist, mp3_link) VALUES (?, ?, ?)", (title, artist, mp3_link))
+            conn.commit()
+            print("success")
+            return True
+        except sqlite3.Error as e:
+            print(f"Error adding music: {e}")
+            return False
 
 if __name__ == "__main__":
     db = MusicPlayerDatabase()
+    db.add_music("Judas", "Lady Gaga", "abc")
